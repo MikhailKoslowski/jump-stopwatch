cda9b6edb883244028ed082065874286bd8f2ec3
Mikhail <mikhail.koslowsk@gmail.com>
2021-03-29 19:41:42 -0300

    started refactoring widgets to fix performance issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
-import 'dart:io';
+import 'dart:math';
 import 'dart:typed_data';
-//import 'package:audioplayers/audioplayers.dart';
+
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
+import 'package:sensors/sensors.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,44 +29,47 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Stopwatch with fall detection'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Stopwatch with fall detection')),
+        body: MyHomePage(),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title = ''}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // StopWatch
   final stopWatch = Stopwatch();
   Timer? refreshTimer;
+
+  // Countdown timer
   int countdown = 3;
   bool countingDown = false;
   bool transparent = true;
   int animationDuration = 250;
-  static AudioCache sounds = AudioCache();
 
+  // Sound fx
+  static AudioCache sounds = AudioCache();
   final _440 = '440.wav';
   final _880 = '880.wav';
-
   late Uint8List _440_bytes;
   late Uint8List _880_bytes;
 
+  // accelerometer
+  List<StreamSubscription<dynamic>> _streamSubscriptions =
+      <StreamSubscription<dynamic>>[];
+  double acceleration = 0.0;
+  final double gravity = 9.6;
+  double threshold = 10.0;
+  double maxAccel = 0.0;
+  final twoDigitsDouble = NumberFormat('00.00');
 
   void loadSounds() async {
     //await sounds.loadAll([_440, _880]);
@@ -72,19 +77,40 @@ class _MyHomePageState extends State<MyHomePage> {
     _880_bytes = (await sounds.load(_880)).readAsBytesSync();
   }
 
-  _MyHomePageState() {
+  @override
+  void initState() {
+    super.initState();
     loadSounds();
+    //accelerometerEvents.listen(accelerometerCallback);
+    _streamSubscriptions
+        .add(accelerometerEvents.listen((AccelerometerEvent data) {
+      //acceleration = sqrt(data.x * data.x + data.y * data.y + data.z * data.z) - gravity;
+      acceleration = max(max(data.x.abs(), data.y.abs()), data.z.abs()) - gravity;
+
+      if (acceleration > threshold && stopWatch.isRunning) {
+        sounds.playBytes(_440_bytes);
+        stop();
+      }
+    }));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (StreamSubscription<dynamic> subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
   }
 
   void animateCountdown() {
     setState(() {
-      sounds.playBytes(countdown > 0 ?_440_bytes : _880_bytes);
-      animationDuration = 50;
+      sounds.playBytes(countdown > 0 ? _440_bytes : _880_bytes);
+      animationDuration = 100;
       transparent = false;
     });
     Future.delayed(Duration(milliseconds: animationDuration), () {
       setState(() {
-        animationDuration = 900;
+        animationDuration = 650;
         transparent = true;
       });
     });
@@ -98,10 +124,26 @@ class _MyHomePageState extends State<MyHomePage> {
     } else {
       countingDown = false;
       stopWatch.start();
-      refreshTimer = Timer.periodic(Duration(milliseconds: 50), (timer) {
-        setState(() {});
+      refreshTimer = Timer.periodic(Duration(milliseconds: 100), (timer) {
+        setState(() {}); // redraw stopwatch
       });
     }
+  }
+
+  void start() {
+    countingDown = true;
+    countdown = 3;
+    stopWatch.reset();
+    animateCountdown();
+    refreshTimer = Timer(Duration(seconds: 1), runCountdown);
+  }
+
+  void stop() {
+    setState(() {
+      countingDown = false;
+      stopWatch.stop();
+      refreshTimer?.cancel();
+    });
   }
 
   @override
@@ -112,13 +154,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
+    return Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
@@ -136,7 +172,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             /*Text(
               'You have pushed the button this many times:',
@@ -145,6 +181,27 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               //style: Theme.of(context).textTheme.headline4,
             ),*/
+
+            //Text(
+            //  'Current acceleration: ${twoDigitsDouble.format(acceleration)}',
+            //  style: Theme.of(context).textTheme.headline5,
+            //),
+            Text(
+              'Threshold for stop: ${twoDigitsDouble.format(threshold)} m/s²',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Slider(
+              value: threshold,
+              min: 0.0,
+              max: 20.0,
+              label: threshold.toString(),
+              onChanged: (value) {
+                setState(() {
+                  threshold = value;
+                });
+              },
+            ),
+            //Text('$acceleration'),
             AnimatedDefaultTextStyle(
                 child: Text(countdown.toString()),
                 style: transparent
@@ -163,29 +220,18 @@ class _MyHomePageState extends State<MyHomePage> {
               '${stopWatch.elapsed}',
               style: Theme.of(context).textTheme.headline5,
             ),
+            IconButton(onPressed: () {
+              if (!stopWatch.isRunning && !countingDown) {
+                start();
+              } else {
+                stop();
+              }
+            },
+              icon: Icon(stopWatch.isRunning || countingDown
+                  ? Icons.pause
+                  : Icons.play_arrow),)
           ],
         ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (!stopWatch.isRunning && !countingDown) {
-            countingDown = true;
-            countdown = 5;
-            stopWatch.reset();
-            animateCountdown();
-            refreshTimer = Timer(Duration(seconds: 1), runCountdown);
-          } else {
-            setState(() {
-              countingDown = false;
-              stopWatch.stop();
-              refreshTimer?.cancel();
-            });
-          }
-        },
-        tooltip: 'Increment',
-        child: Icon(stopWatch.isRunning || countingDown ? Icons.pause : Icons.play_arrow),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
   }
 }
